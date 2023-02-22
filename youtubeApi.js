76fd4b3a5722fcc7925f916aa494f9054b73df21
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-22 23:42:12 +0100

    deploy

--- youtubeApi.js
+++
@@ -1,7 +0,0 @@
-const axios=require("axios");
-
-exports.Search=async(query)=>{
-    const response=await axios.get("https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q="+query+"&key=AIzaSyBFYvy18_rgbpcfoJNpNLvWzdtXMnlPK50");
-    videoId=response.data.items[0].id.videoId;
-    return videoId;
-}
