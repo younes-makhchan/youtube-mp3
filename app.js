5eb6b5524b93950c2b6dd61e03c3d88a7f876112
Younes Makhchan <younessmakhchane@gmail.com>
2023-02-22 23:37:19 +0100

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const bodyParser=require("body-parser")
+const path=require("path");
+const youtubeRouter=require("./routes/youtube")
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs");
+app.set("views","./views");
+
+
+app.use("/",youtubeRouter)
+
+app.listen(3000);
